30e94ae9e16c85756adcdcf0f34bb0d8548c0a99
Vibhoo Vishwakarma <121712735+VIBHOO12@users.noreply.github.com>
2025-06-06 08:54:19 +0530

    exe3.js

--- Daily/day8/exe3.js
+++ Daily/day8/exe3.js
@@ -1,12 +1,10 @@
-let arr = [4,44,4,8,7,9,2,7,2];
-let dublicatesCount = 0;
-let dublicates = [];
-for(let i=0; i<arr.length; i++){
-    if(arr.includes(i)== arr.includes(i-1)){
-        dublicatesCount++;
-        dublicates.push(arr[i]);
-    }
-}
+let arr = [1, 2, 3, 2, 4, 5, 1, 6, 7, 3];
 
-console.log("count of dublicte:", dublicatesCount);
-console.log("number", dublicates);
+let duplicates = arr.filter((item, index) => {
+  return arr.indexOf(item) !== index;
+});
+
+// To remove repeated duplicates
+let uniqueDuplicates = [...new Set(duplicates)];
+
+console.log("Duplicate numbers:", uniqueDuplicates);
