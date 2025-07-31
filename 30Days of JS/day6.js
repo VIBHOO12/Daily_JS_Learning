600f9630d9314b31f4d86ed5c5e6198360db0b9b
Vibhoo Vishwakarma <dipshikhavishwakarma0@gmail.com>
2025-07-31 20:20:06 +0530

    Added day1.html and day6.js

---
+++ 30Days of JS/day6.js
@@ -0,0 +1,45 @@
+function memoize(fn) {
+    const cache = new Map(); // cache store
+
+    return function(...args) {
+        const key = args.join(","); // key बनाना
+
+        if (cache.has(key)) {
+            return cache.get(key); // if present then return
+        }
+
+        const result = fn(...args); // if not then calculate
+        cache.set(key, result); // store in cache
+        return result;
+    };
+}
+
+// Example functions
+function sum(a, b) {
+    return a + b;
+}
+
+function fib(n) {
+    if (n <= 1) return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+function factorial(n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
+}
+
+// Memoized versions
+const memoizedSum = memoize(sum);
+const memoizedFib = memoize(fib);
+const memoizedFactorial = memoize(factorial);
+
+console.log(memoizedSum(2, 3)); //5
+console.log(memoizedSum(2, 3)); //return from cache 5
+console.log(memoizedSum(3, 3)); //new calculate 
+
+console.log(memoizedFib(5));    
+console.log(memoizedFib(5));    
+
+console.log(memoizedFactorial(5)); 
+console.log(memoizedFactorial(5));
