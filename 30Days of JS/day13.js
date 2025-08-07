e5f205aecf78da799ea784a1d1008f5bd853110e
Vibhoo Vishwakarma <dipshikhavishwakarma0@gmail.com>
2025-08-07 23:09:24 +0530

    Updated day12.js and Added day13.js

---
+++ 30Days of JS/day13.js
@@ -0,0 +1,16 @@
+// 2627. Debounce - LeetCode
+
+var debounce = function(fn, t) {
+    let timeoutId = null;
+    return function(...args) {
+        if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn(...args); 
+    }, t);
+    }
+};
+
+ const log = debounce(console.log, 100);
+ log('Hello'); // cancelled
+ log('Hello'); // cancelled
+ log('Hello'); // Logged at t=100ms
